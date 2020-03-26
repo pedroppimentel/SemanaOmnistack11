ece2bf4ca4b769eb615d70bc9a80086899bd002e
Pedro P Pimentel <pedrop.pimentel@hotmail.com>
2020-03-26 00:06:19 -0300

    Front-end  do app e alteracao condicao do back-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes'; //Quando importa uma pasta sempre é procurado o arquivo index.js primeiramente
 
 function App() {
+  //quando precisar armazenar info usando react, cria um estado ao inves da variavel
+  //const[counter, setCounter] = useState(0);
+
+  // useState retorna -> Array[valor, função de atualização]
+
+  /*function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={ increment }>Incrementar</button>
     </div>
+  );*/
+
+  return (
+    <Routes />
   );
 }
 
